5f8ca067d054a949081e7db73f65d0277992d624
chan <986311827@qq.com>
2025-03-18 16:44:18 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from './pages/Home/Home'
+
+const App = () => {
+ 
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+ 
+};
+
+export default App;
